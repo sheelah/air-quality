b4e0aaff112006c3a13c4e1010307903f21b8fe0
Sheelah Brennan <slb53@hotmail.com>
2020-08-20 18:10:30 -0600

    Add components to index component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import Head from 'next/head';
-import Footer from '../components/footer';
+import AirQuality from '../components/AirQuality';
+import Footer from '../components/Footer';
+import LocationForm from '../components/LocationForm';
+import { LocationContext } from '../components/LocationContext';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [location, setLocation] = useState({ lat: null, lng: null });
+  const [airQualityIndex, setAirQualityIndex] = useState({});
+  const [isFetching, setIsFetching] = useState(false);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +20,21 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Air Quality Today</h1>
+
+        <LocationContext.Provider
+          value={{
+            location,
+            setLocation,
+            airQualityIndex,
+            setAirQualityIndex,
+            isFetching,
+            setIsFetching,
+          }}
+        >
+          <LocationForm />
+
+          <AirQuality />
+        </LocationContext.Provider>
       </main>
 
       <Footer />
