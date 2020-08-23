8e7d707121b090b3b482a2c95c03a3d05c84c210
Sheelah Brennan <slb53@hotmail.com>
2020-08-22 18:26:46 -0600

    Add air quality component

--- components/AirQuality.js
+++ components/AirQuality.js
@@ -1,13 +1,37 @@
-import { useState, useEffect } from 'react';
 import { useLocationContext } from './LocationContext';
+import { useWeatherByCoords } from '../hooks/useWeatherData';
+import { getPm25Levels } from '../utils/dataFilters';
 
 const AirQuality = () => {
-  const { location } = useLocationContext();
+  const { locationCoords } = useLocationContext();
+  const { isLoading, isError, data, error } = useWeatherByCoords(
+    locationCoords.lat,
+    locationCoords.lng
+  );
+
+  if (!data || isLoading) {
+    return 'Loading...';
+  }
+
+  if (isError) {
+    console.log('Oops - an error occurred:', error);
+  }
+
+  // Filter air quality for small particulate matter levels
+  const aq = getPm25Levels(data);
+
+  const {
+    AQI: airQualityIndex,
+    ReportingArea: reportingLocation,
+    Category: { Name: description },
+  } = aq;
 
   return (
-    <h1>
-      Air Quality for <pre>{JSON.stringify(location)}</pre>:
-    </h1>
+    <div className="air-quality-status">
+      <span className="air-quality-index">{airQualityIndex}</span>
+      <span className="reporting-location">{reportingLocation}</span>
+      <span className="description">{description}</span>
+    </div>
   );
 };
 
