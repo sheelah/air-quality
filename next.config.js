f49c9ebf57ebe63bd277aa4397036feb538dea1c
Sheelah Brennan <slb53@hotmail.com>
2020-08-22 19:57:01 -0600

    Add SVG support

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+const path = require('path');
+const withImages = require('next-images')
+module.exports = withImages({
+  exclude: path.resolve(__dirname, 'src/assets/svg'),
+  webpack(config, options) {
+    return config
+  }
+})
